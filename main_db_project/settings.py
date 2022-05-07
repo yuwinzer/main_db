996433441865b8b2e1a995b11e431d3360e90f73
yuwinzer <yu.moskalev@gmail.com>
2022-05-08 01:03:01 +0300

    removed secret key from public

--- main_db_project/settings.py
+++ main_db_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config # for sensitive data .env
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@a8zfpfiq!&v57$^0za#$8vdnb)&oj@h_#rqy_8x3jc3)#$2)b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
