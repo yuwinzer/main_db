96875872c7e16f2dc9ded172ac2c515224f0f46c
yuwinzer <yu.moskalev@gmail.com>
2022-09-26 23:05:18 +0300

    renamed django project, changed urls to apps pages

--- apps/components/urls.py
+++ apps/components/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [path("components", views.index)]
--- apps/finance/urls.py
+++ apps/finance/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [path("finance", views.index)]
